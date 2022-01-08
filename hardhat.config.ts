8627c37d6797ebab90786b9f0d0e94481d76a626
arturpezda <atipezda@gmail.com>
2022-01-08 19:10:08 +0100

    FEAT: Ropsten deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,18 @@
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
 import "@typechain/hardhat";
+require('dotenv').config()
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY
+
+
 module.exports = {
-  solidity: "0.7.3",
+    solidity: "0.7.3",
+    networks: {
+        ropsten: {
+            url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+            accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+        }
+    }
 };
