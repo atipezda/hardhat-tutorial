7468d0170c5f3007d75bae8268c83547d48baab4
arturpezda <atipezda@gmail.com>
2022-01-08 14:13:18 +0100

    TEST: updated transfer test

--- test/Token.js
+++ test/Token.js
@@ -15,12 +15,18 @@ describe("Token contract", function () {
 	})
 
 	it("Should transfer tokens between accounts", async function () {
-		const [owner, addr1, addr2] = await ethers.getSigners();
+		const TRANSACTION_AMOUNT = 50;
+
+		const [owner, user1] = await ethers.getSigners();
 		const Token = await ethers.getContractFactory("Token");
 		const hardhatToken = await Token.deploy();
 
+		const ownerBalance = await hardhatToken.balanceOf(owner.address);
 
-		await hardhatToken.transfer(addr1.address, 50);
-		expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+		await hardhatToken.transfer(user1.address, TRANSACTION_AMOUNT);
+		const ownerAfterTransferBalance = ownerBalance - TRANSACTION_AMOUNT;
+
+		expect(await hardhatToken.balanceOf(user1.address)).to.equal(TRANSACTION_AMOUNT);
+		expect(await hardhatToken.balanceOf(owner.address)).to.equal(ownerAfterTransferBalance);
 	})
 })
