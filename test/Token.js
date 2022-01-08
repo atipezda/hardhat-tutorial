1040a955aa74cb41563e6bf9c2d50619186366a1
arturpezda <atipezda@gmail.com>
2022-01-08 14:07:31 +0100

    FEAT: ATP Token and basic tests

---
+++ test/Token.js
@@ -0,0 +1,26 @@
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+
+describe("Token contract", function () {
+	it("Deployment should assign all tokens to owner", async function () {
+		const [owner] = await ethers.getSigners();
+
+		const Token = await ethers.getContractFactory("Token");
+
+		const hardhatToken = await Token.deploy();
+
+		const ownerBalance = await hardhatToken.balanceOf(owner.address);
+		expect(await hardhatToken.totalSupply()).to.equal(ownerBalance)
+
+	})
+
+	it("Should transfer tokens between accounts", async function () {
+		const [owner, addr1, addr2] = await ethers.getSigners();
+		const Token = await ethers.getContractFactory("Token");
+		const hardhatToken = await Token.deploy();
+
+
+		await hardhatToken.transfer(addr1.address, 50);
+		expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+	})
+})
